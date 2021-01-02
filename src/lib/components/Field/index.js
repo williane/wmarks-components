cb394ff2f861916e5783d7316fbbb0dfa5ca6c46
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2021-01-02 13:06:41 -0300

    💄add required and value on Field

--- src/lib/components/Field/index.js
+++ src/lib/components/Field/index.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Label, Content, Input } from './style'
 
-function Field({ label, placeholder, onChange }) {
+function Field({ label, placeholder, onChange, value, readOnly, required }) {
   return (
     <Label>
       <Content>{label}</Content>
-      <Input type="text" placeholder={placeholder} onChange={onChange} />
+      {value ? (
+        <Input
+          type="text"
+          placeholder={placeholder}
+          onChange={onChange}
+          value={value}
+          readOnly={readOnly}
+          required={required}
+        />
+      ) : (
+        <Input
+          type="text"
+          placeholder={placeholder}
+          onChange={onChange}
+          required={required}
+        />
+      )}
     </Label>
   )
 }
--- src/stories/Field.stories.js
+++ src/stories/Field.stories.js
@@ -3,12 +3,24 @@ import { Field } from '../lib'
 
 export default {
   title: 'Components/Field',
-  component: Field
+  component: Field,
+  argTypes: {
+    onChange: { action: 'clicked' }
+  }
 }
 
 const Template = (args) => <Field {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-  label: 'Name'
+  label: 'Name',
+  required: false
+}
+
+export const Value = Template.bind({})
+Value.args = {
+  label: 'Name',
+  value: 'Texto aqui',
+  readOnly: true,
+  required: true
 }
