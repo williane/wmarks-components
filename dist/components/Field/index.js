04b5a9552bec9a86105009ecef628de873d5596b
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2021-01-02 13:09:00 -0300

    🔖new version 0.1.9

--- dist/components/Field/index.js
+++ dist/components/Field/index.js
@@ -14,11 +14,22 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function Field(_ref) {
   var label = _ref.label,
       placeholder = _ref.placeholder,
-      onChange = _ref.onChange;
-  return /*#__PURE__*/_react.default.createElement(_style.Label, null, /*#__PURE__*/_react.default.createElement(_style.Content, null, label), /*#__PURE__*/_react.default.createElement(_style.Input, {
+      onChange = _ref.onChange,
+      value = _ref.value,
+      readOnly = _ref.readOnly,
+      required = _ref.required;
+  return /*#__PURE__*/_react.default.createElement(_style.Label, null, /*#__PURE__*/_react.default.createElement(_style.Content, null, label), value ? /*#__PURE__*/_react.default.createElement(_style.Input, {
     type: "text",
     placeholder: placeholder,
-    onChange: onChange
+    onChange: onChange,
+    value: value,
+    readOnly: readOnly,
+    required: required
+  }) : /*#__PURE__*/_react.default.createElement(_style.Input, {
+    type: "text",
+    placeholder: placeholder,
+    onChange: onChange,
+    required: required
   }));
 }
 
