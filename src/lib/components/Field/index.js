72d8ab8d4488728fc6c1a95350c1ba88259db7aa
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2021-01-02 11:57:23 -0300

    ✨ add event onChange on Field

--- src/lib/components/Field/index.js
+++ src/lib/components/Field/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Label, Content, Input } from './style'
 
-function Field({ label, placeholder }) {
+function Field({ label, placeholder, onChange }) {
   return (
     <Label>
       <Content>{label}</Content>
-      <Input type="text" placeholder={placeholder} />
+      <Input type="text" placeholder={placeholder} onChange={onChange} />
     </Label>
   )
 }
